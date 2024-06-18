cb3c286be85726114b023b8488dee72061e8e82c
Hannes <Hannes.o@t-online.de>
2024-06-18 12:36:00 +0200

    create a test file for python-database connection

---
+++ applications/databaseConnection.py
@@ -0,0 +1,56 @@
+#python: pip install cx_Oracle
+#Desktopentwicklung für C++ könnte notwendig sein
+#Oracle Client muss heruntergeladen und in die Systemvariablen eingetragen sein: https://www.oracle.com/database/technologies/instant-client/winx64-64-downloads.html
+
+
+import cx_Oracle
+
+
+#global variables for the database connection
+dsn = cx_Oracle.makedsn("rs03-db-inf-min.ad.fh-bielefeld.de", 1521, sid="orcl")
+user = "AstroSphere"
+password = "astrosphere"
+
+
+
+
+
+def execute_sql_query(sql_query):
+    global dsn
+    global user
+    global password
+
+    connection = None
+    cursor = None
+
+    print("User: ", user, "; Password: ", password, "; DSN: ", dsn)
+    try:
+        connection = cx_Oracle.connect(user=user, password=password, dsn=dsn, encoding="UTF-8")
+        cursor = connection.cursor()
+
+        cursor.execute(sql_query)
+        
+        results = cursor.fetchall()
+
+        for row in results:
+            print(row)
+        
+        return results
+    
+    except cx_Oracle.DatabaseError as e:
+        error, = e.args
+        print("Datenbankfehler:", error.message)
+    except Exception as e:
+        print("Allgemeiner Fehler:", str(e))
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+#check if cx_Oracle works
+print(cx_Oracle.clientversion())
+
+#sql-example
+sql_query = "SELECT * FROM ABTEILUNG"
+execute_sql_query(sql_query)
