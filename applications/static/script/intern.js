12285dab77cdd632313d060ee729758a469b8e3a
Max Steidle <maxsteidleweingard@icloud.com>
2024-06-19 22:11:49 +0200

    Adding the navigation function between the different pages

--- applications/static/script/intern.js
+++ applications/static/script/intern.js
@@ -1,15 +1,55 @@
-function goBackHome() {
-    window.location.href = "{{ url_for('back_to_home') }}";
+function showSection(sectionId) {
+    // Alle Inhaltsbereiche ausblenden
+    var sections = document.querySelectorAll('.content-section');
+    sections.forEach(function(section) {
+        section.classList.remove('active');
+    });
+
+    // Den ausgewählten Inhaltsbereich anzeigen
+    var selectedSection = document.getElementById(sectionId);
+    selectedSection.classList.add('active');
 }
 
-function activateButton(button) {
-    console.log('activate');
-    // Alle Buttons in der Gruppe deaktivieren
-    var buttons = document.querySelectorAll('.Nav_Btn');
-    buttons.forEach(function(btn) {
-        btn.classList.remove('active');
+function showLeftPanel(panelId) {
+    // Alle Inhaltsbereiche ausblenden
+    var sections = document.querySelectorAll('.MainLeftContent');
+    sections.forEach(function(section) {
+        section.classList.remove('active');
     });
-    
-    // Den geklickten Button aktivieren
-    button.classList.add('active');
+
+    // Den ausgewählten Inhaltsbereich anzeigen
+    var selectedSection = document.getElementById(panelId);
+    selectedSection.classList.add('active');
+}
+
+function navBar(event, sectionId, panelId) {
+    event.preventDefault();
+
+    // Alle Navigationslinks deaktivieren
+    var navLinks = document.querySelectorAll('.nav-links a');
+    navLinks.forEach(function(link) {
+        link.classList.remove('active');
+    });
+
+    // Den geklickten Navigationslink aktivieren
+    event.target.classList.add('active');
+
+    showSection(sectionId);
+
+    showLeftPanel(panelId);
+}
+
+function filter(event, sectionId) {
+    event.preventDefault();
+
+    // Alle Navigationslinks deaktivieren
+    var navLinks = document.querySelectorAll('.MainLeftContent a');
+    navLinks.forEach(function(link) {
+        link.classList.remove('active');
+    });
+
+    // Den geklickten Navigationslink aktivieren
+    event.target.classList.add('active');
+
+    showSection(sectionId);
 }
