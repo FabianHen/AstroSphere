a5e85e43129b99a60327bc9291e2f44112128ecc
Max Steidle <maxsteidleweingard@icloud.com>
2024-06-20 22:26:56 +0200

    Implementation of the media elements

--- applications/static/script/intern.js
+++ applications/static/script/intern.js
@@ -10,6 +10,48 @@ function showSection(sectionId) {
     selectedSection.classList.add('active');
 }
 
+// Event-Listener für DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function() {
+    const mediaItem = document.getElementById('mediaGrid');
+    if (mediaItem) {
+        loadMedia(mediaItem);
+    }
+});
+
+function loadMedia(elem) {
+    // Datenbank lesen
+    let testMedia = [['Mond', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Erde', '*.png', '../static/images/pictureNotFound.png'],
+                     ['Mars', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Jupiter', '*.jpg', '../static/images/pictureNotFound.png'],
+                     ['Saturn', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Venus', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Merkur', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Neptun', '*.svg', '../static/images/pictureNotFound.png'],
+                     ['Test', '*.svg', '../static/images/pictureNotFound.png']]
+    
+    for (var i = 0; i < testMedia.length; i++) {
+        var htmlElem =  "<a href='#' class='media-item' onclick='mediaPressed(this)'>"
+        htmlElem += "<img src=" + testMedia[i][2] + " alt='Media 1'>"
+        htmlElem += "<h3>" + testMedia[i][0] + "</h3>"
+        htmlElem += "<p>" + testMedia[i][1] + "</p>"
+        htmlElem += "</a>"
+
+        elem.innerHTML += htmlElem;
+    }
+}
+
 function filter(event, sectionId) {
     event.preventDefault();
 
@@ -23,6 +65,10 @@ function filter(event, sectionId) {
     event.target.classList.add('active');
 
     showSection(sectionId);
+}
+
+function mediaPressed(elem){
+    elem.classList.toggle('active');
 }
 
 function goToEvents() {
