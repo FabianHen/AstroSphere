15e411de27fe400ccbe5ccd8cd3ee334b6996b26
Hannes <Hannes.o@t-online.de>
2024-06-23 21:23:41 +0200

    shopping Cart; -> div panel for shopping cart -> buttons for cancel and buy items -> onclick functions (empty)

--- applications/static/script/shop.js
+++ applications/static/script/shop.js
@@ -1,3 +1,14 @@
 function goBack() {
     window.location.href = window.location.href.split('/', window.location.href.split('/').length-1).join('/');
 }
+
+
+
+
+
+function cancel_Order(){
+    console.log("cancel Order");
+}
+function buy_Order(){
+    console.log("buy Order");
+}
