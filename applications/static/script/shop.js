fa148932293bccf4182ed2c63e367e05c9775bd0
Hannes <Hannes.o@t-online.de>
2024-06-23 21:36:38 +0200

    first shopping Cart functions: -> global array for items -> class Artikel represents items -> addItemToCart function (params: type, anzahl, preis) -> removeItem (type, anzahl)

--- applications/static/script/shop.js
+++ applications/static/script/shop.js
@@ -5,10 +5,47 @@ function goBack() {
 
 
 
+const shoppingCart=[];
+
+class Artikel {
+    constructor(type, anzahl, preis) {
+        this.type = type;
+        this.anzahl = anzahl;
+        this.preis = preis;
+    }
+}
+
+function addItemToCart(type, anzahl, preis){
+    aktItem = new Artikel(type, anzahl, preis);
+    for (let i = 0; i < shoppingCart.length; i++) {
+        const item = shoppingCart[i];
+        if (item.type === type) {
+            item.anzahl += anzahl;
+            return;
+        }
+    }
+    shoppingCart.push(aktItem);
+}
+
+function removeItemFromCart(type, anzahl){
+    for (let i = 0; i < shoppingCart.length; i++) {
+        const item = shoppingCart[i];
+        if (item.type === type) {
+            item.anzahl -= anzahl;
+            if (item.anzahl <= 0) {
+                shoppingCart.splice(i, 1);
+            }
+            return;
+        }
+    }
+    console.log(`Artikel ${type} nicht gefunden.`);
+}
 
 function cancel_Order(){
     console.log("cancel Order");
+    shoppingCart=[];
 }
 function buy_Order(){
     console.log("buy Order");
 }
+
