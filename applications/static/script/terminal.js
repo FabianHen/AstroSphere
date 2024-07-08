f084f837cbeab2b3ce8a7d8a4c2a64979e140b54
HOlschok <holschok@gauselmann.de>
2024-07-08 11:58:31 +0200

    a little advertisement page on terminal (appears after a short time of no user interaction)

--- applications/static/script/terminal.js
+++ applications/static/script/terminal.js
@@ -5,3 +5,35 @@ function goBackHome() {
 function goToShop(element) {
     window.location.href += `/shop?selected=${element}`;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var advertisement = document.getElementById("advertisement");
+    var adImage = document.getElementById("adImage");
+    var imageSources = [
+        "../static/images/advertisement_1.png",
+        "../static/images/advertisement_2.png",
+        "../static/images/advertisement_3.png"
+    ];
+    var currentIndex = 0;
+    var intervalId;
+
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % imageSources.length;
+        adImage.src = imageSources[currentIndex];
+    }
+
+    function startInterval() {
+        intervalId = setInterval(function() {
+            showNextImage();
+            advertisement.style.display="flex";
+        }, 5000);
+    }
+
+    document.addEventListener("click", function() {
+        advertisement.style.display="none";
+        clearInterval(intervalId);
+        setTimeout(startInterval, 15000);
+    });
+
+    startInterval();
+});
